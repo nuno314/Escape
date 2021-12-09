34b6f3887f490d1d0a86073c38c6be4ed3772149
thientamdao <daothithientam@gmail.com>
2021-12-09 12:45:10 +0700

    update gameui

---
+++ core/src/com/mygdx/screens/SettingScreen.java
@@ -0,0 +1,113 @@
+package com.mygdx.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.Escape;
+
+public class SettingScreen implements Screen {
+
+    private Escape game;
+    private SpriteBatch batch;
+    private Stage stage;
+    private Skin skin;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+
+    public SettingScreen() {
+        skin = new Skin(Gdx.files.internal("skin/screen.json"), new TextureAtlas("skin/screen.pack"));
+
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(Escape.WIDTH, Escape.HEIGHT, camera);
+
+        viewport.apply();
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        stage = new Stage(viewport, batch);
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+
+        Button home = new Button(skin, "home_off");
+        Button rank = new Button(skin, "rank_off");
+        Button setting = new Button(skin, "setting");
+
+        home.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new ConnectScreen(game));
+            }
+        });
+
+        rank.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new RankScreen());
+            }
+        });
+
+        Table menu = new Table();
+        menu.left();
+        menu.add(home);
+        menu.add(rank);
+        menu.add(setting);
+
+        Table root = new Table();
+        root.setBackground(skin.getDrawable("update_later"));
+        root.setFillParent(true);
+        root.top();
+        root.add(menu).expand().bottom();
+
+        stage.addActor(root);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        skin.dispose();
+    }
+}
